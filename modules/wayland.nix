898b01f6bc8ff7e4c17fd6f2fc695b9668038835
Fazzi <faaris.ansari@proton.me>
2025-01-02 23:31:05 +0000

    add nix overlay to bump pywalfox-native to 2.8.0rc1

--- modules/wayland.nix
+++ modules/wayland.nix
@@ -55,6 +55,16 @@
       ags = prev.ags.overrideAttrs (old: {
         buildInputs = old.buildInputs ++ [ pkgs.libdbusmenu-gtk3 ];
       });
+      # Make pywalfox use the more recent ver
+      pywalfox-native = prev.python3.pkgs.buildPythonApplication {
+        pname = "pywalfox-native";
+        version = "2.8.0rc1";
+
+        src = prev.fetchurl {
+          url = "https://test-files.pythonhosted.org/packages/89/a1/8e011e2d325de8e987f7c0a67222448b252fc894634bfa0d3b3728ec6dbf/pywalfox-2.8.0rc1.tar.gz";
+          sha256 = "89e0d7a441eb600933440c713cddbfaecda236bde7f3f655db0ec20b0ae12845";
+        };
+      };
     })
   ];
 }
