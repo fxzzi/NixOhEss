67600b0f250ea1e3dea0212000a2699848785a97
Fazzi <faaris.ansari@proton.me>
2025-01-05 12:10:47 +0000

    fixup fancontrol, idk why the hwmon[n]'s changed, also add something to hyprland conf to make some parts less repetitive

--- home/gui/hypr/hyprland.nix
+++ home/gui/hypr/hyprland.nix
@@ -249,37 +249,23 @@
 				"$MOD SHIFT, right, movewindow, r"
 				"$MOD SHIFT, up, movewindow, u"
 				"$MOD SHIFT, down, movewindow, d"
-
-				# switch workspace
-				"$MOD, 1, workspace, 1"
-				"$MOD, 2, workspace, 2"
-				"$MOD, 3, workspace, 3"
-				"$MOD, 4, workspace, 4"
-				"$MOD, 5, workspace, 5"
-				"$MOD, 6, workspace, 6"
-				"$MOD, 7, workspace, 7"
-				"$MOD, 8, workspace, 8"
-				"$MOD, 9, workspace, 9"
-				"$MOD, 0, workspace, 10"
-				"$MOD ALT, up, workspace, e+1"
-				"$MOD ALT, down, workspace, e-1"
-
-				# move window to workspace
-				"$MOD SHIFT, 1, movetoworkspace, 1"
-				"$MOD SHIFT, 2, movetoworkspace, 2"
-				"$MOD SHIFT, 3, movetoworkspace, 3"
-				"$MOD SHIFT, 4, movetoworkspace, 4"
-				"$MOD SHIFT, 5, movetoworkspace, 5"
-				"$MOD SHIFT, 6, movetoworkspace, 6"
-				"$MOD SHIFT, 7, movetoworkspace, 7"
-				"$MOD SHIFT, 8, movetoworkspace, 8"
-				"$MOD SHIFT, 9, movetoworkspace, 9"
-				"$MOD SHIFT, 0, movetoworkspace, 10"
-
 				# navigate through workspaces on mouse
+
 				"$MOD, mouse_down, workspace, e+1"
 				"$MOD, mouse_up, workspace, e-1"
-			];
+			]
+			++ (
+        # workspaces
+        # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+        builtins.concatLists (builtins.genList (i:
+            let ws = i + 1;
+            in [
+              "$MOD, code:1${toString i}, workspace, ${toString ws}"
+              "$MOD SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+            ]
+          )
+          9)
+      );
 
 			binde = [
 				# volume script
@@ -314,13 +300,15 @@
 			# 	watchdog_timeout = 0;
 			# };
 		};
-# # submaps
-# # Disables all keybinds for moonlight or vm's
+		extraConfig = ''
+# submaps
+# Disables all keybinds for moonlight or vm's
 # bind = $MOD SHIFT, N, submap, clean
 # submap = clean
 # bind = $MOD SHIFT, N, submap, reset
 # submap = reset
-  };
+		'';
+	};
   imports = [
     ./env.nix
   ];
