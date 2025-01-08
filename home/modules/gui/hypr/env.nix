92d7bdc8ca2f3b54922a47dc8fccde8e3d8e5335
Fazzi <faaris.ansari@proton.me>
2025-01-08 15:45:10 +0000

    only enable nvidia env vars if host is fazziPC

--- home/modules/gui/hypr/env.nix
+++ home/modules/gui/hypr/env.nix
@@ -1,26 +1,28 @@
-{ ... }:
+{ hostName, lib, ... }:
 {
-  home.sessionVariables = {
-    # run electron, gtk, qt apps in wayland native
-    NIXOS_OZONE_WL = "1";
-    ELECTRON_OZONE_PLATFORM_HINT = "auto";
-    GDK_BACKEND = "wayland,x11";
-    QT_QPA_PLATFORM = "wayland;xcb";
+  home.sessionVariables = lib.mkMerge [
+    {
+      # run electron, gtk, qt apps in wayland native
+      NIXOS_OZONE_WL = "1";
+      ELECTRON_OZONE_PLATFORM_HINT = "auto";
+      GDK_BACKEND = "wayland,x11";
+      QT_QPA_PLATFORM = "wayland;xcb";
 
-    # fix java bug on tiling wm's / compositors
-    _JAVA_AWT_WM_NONREPARENTING = "1";
+      # fix java bug on tiling wm's / compositors
+      _JAVA_AWT_WM_NONREPARENTING = "1";
+    }
+    (lib.mkIf (hostName == "fazziPC") {
+      # nvidia shenanigans
+      LIBVA_DRIVER_NAME = "nvidia";
+      GBM_BACKEND = "nvidia-drm";
+      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+      NVD_BACKEND = "direct";
+      MOZ_DISABLE_RDD_SANDBOX = "1";
+      __GL_SYNC_TO_VBLANK = "0";
 
-    # nvidia shenanigans
-    # LIBVA_DRIVER_NAME = "nvidia";
-    # GBM_BACKEND = "nvidia-drm";
-    # __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-    # NVD_BACKEND = "direct";
-    # MOZ_DISABLE_RDD_SANDBOX = "1";
-    # __GL_SYNC_TO_VBLANK = "0";
-
-    # enable these when nvidia gets their shit together
-    # __GL_GSYNC_ALLOWED = "0";
-    # __GL_VRR_ALLOWED = "0";
-
-  };
+      # enable these when nvidia gets their shit together
+      __GL_GSYNC_ALLOWED = "0";
+      __GL_VRR_ALLOWED = "0";
+    })
+  ];
 }
