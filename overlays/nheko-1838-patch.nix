309de35e01eb2e74b5c03785dd543f7dd2cd4819
Fazzi <faaris.ansari@proton.me>
2025-01-09 12:09:17 +0000

    alejandra fmt nheko patch

--- overlays/nheko-1838-patch.nix
+++ overlays/nheko-1838-patch.nix
@@ -1,11 +1,10 @@
-{pkgs, ...}:
-{
+{pkgs, ...}: {
   nixpkgs.overlays = [
     (final: prev: {
       nheko = prev.nheko.overrideAttrs (old: {
         patches = pkgs.fetchpatch {
           url = "https://github.com/Nheko-Reborn/nheko/pull/1838.patch";
-					hash = "sha256-hYG/nFPTJSRVadt7H4CgefBCwEAKCR7xU5hA5pTzpXU=";
+          hash = "sha256-hYG/nFPTJSRVadt7H4CgefBCwEAKCR7xU5hA5pTzpXU=";
         };
       });
     })
