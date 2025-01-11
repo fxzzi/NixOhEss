e19581a5c1fb24f70bb9bfe1ba8daf392c7e580b
Fazzi <faaris.ansari@proton.me>
2025-01-11 14:11:04 +0000

    try fix syncthing attempt drei

--- home/modules/apps/syncthing/default.nix
+++ home/modules/apps/syncthing/default.nix
@@ -1,6 +1,7 @@
 {
   user,
   hostName,
+  lib,
   ...
 }: let
   sendOrRecieve =
@@ -8,6 +9,12 @@
     then "sendonly"
     else if hostName == "fazziGO"
     then "recieveonly"
+    else null;
+  otherHost =
+    if hostName == "fazziPC"
+    then "fazziGO"
+    else if hostName == "fazziGO"
+    then "fazziPC"
     else null;
 in {
   config = {
@@ -19,19 +26,19 @@ in {
           "Pissel 7" = {
             id = "E7S5TI3-Z6VYMFB-EL7NZCS-JXQQFMO-7ZQ7U4U-YS6XZLJ-EP7CDON-M4AC7QK";
           };
-          "fazziPC" = {
-            id = "MH7YT2L-5GGIWNU-OXJG2HU-2DPOUWP-NDZ43FK-WSRJXBR-M7I3S33-OVJHQQX";
+          "fazziPC" = lib.mkIf (hostName != "fazziPC") {
+            id = "X47FEBP-ZQCSV2U-SJTHU36-CFJYJHG-Z6W3Y4M-6UEPHRZ-KGDXZQZ-4OQK7AU";
           };
-          "fazziGO" = {
+          "fazziGO" = lib.mkIf (hostName != "fazziGO") {
             id = "364UYNQ-SA7D3BC-DSZWRDU-JIK4I2T-Q4DKAIN-VAFPU33-HAKKJNN-BKXLNQ3";
           };
         };
         folders = {
-          "/home/${user}/Music" = {
+          "~/Music" = {
             id = "music";
             label = "Music";
             type = sendOrRecieve;
-            devices = ["fazziPC" "fazziGO" "Pissel 7"];
+            devices = [otherHost "Pissel 7"];
           };
         };
       };
