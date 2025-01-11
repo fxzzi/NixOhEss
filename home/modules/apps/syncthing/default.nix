d374a019ffb9281763c4ec59cf46983ef8ded182
Fazzi <faaris.ansari@proton.me>
2025-01-11 14:02:45 +0000

    syncthing attempt deux

--- home/modules/apps/syncthing/default.nix
+++ home/modules/apps/syncthing/default.nix
@@ -1,4 +1,15 @@
-{user, ...}: {
+{
+  user,
+  hostName,
+  ...
+}: let
+  sendOrRecieve =
+    if hostName == "fazziPC"
+    then "sendonly"
+    else if hostName == "fazziGO"
+    then "recieveonly"
+    else null;
+in {
   config = {
     services.syncthing = {
       enable = true;
@@ -6,13 +17,21 @@
       settings = {
         devices = {
           "Pissel 7" = {
-						id = "E7S5TI3-Z6VYMFB-EL7NZCS-JXQQFMO-7ZQ7U4U-YS6XZLJ-EP7CDON-M4AC7QK";
-					};
+            id = "E7S5TI3-Z6VYMFB-EL7NZCS-JXQQFMO-7ZQ7U4U-YS6XZLJ-EP7CDON-M4AC7QK";
+          };
+          "fazziPC" = {
+            id = "MH7YT2L-5GGIWNU-OXJG2HU-2DPOUWP-NDZ43FK-WSRJXBR-M7I3S33-OVJHQQX";
+          };
+          "fazziGO" = {
+            id = "364UYNQ-SA7D3BC-DSZWRDU-JIK4I2T-Q4DKAIN-VAFPU33-HAKKJNN-BKXLNQ3";
+          };
         };
         folders = {
           "/home/${user}/Music" = {
             id = "music";
-            devices = ["Pissel 7"];
+            label = "Music";
+            type = sendOrRecieve;
+            devices = ["fazziPC" "fazziGO" "Pissel 7"];
           };
         };
       };
