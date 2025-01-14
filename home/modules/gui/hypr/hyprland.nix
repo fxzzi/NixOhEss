8ccdd8069308bb9e9885e21275fed18523464035
Fazzi <faaris.ansari@proton.me>
2025-01-14 20:20:34 +0000

    remove random ;, it does nothing

--- home/modules/gui/hypr/hyprland.nix
+++ home/modules/gui/hypr/hyprland.nix
@@ -268,7 +268,7 @@ in {
             # binds for apps
             # FIXME: find a better way to define thunar here. this sucks. but alas
             # it works and doesn't make a duplicate package.
-            "$MOD, F, exec, ${lib.getExe' (pkgs.xfce.thunar.override {thunarPlugins = osConfig.programs.thunar.plugins;}) "thunar"};"
+            "$MOD, F, exec, ${lib.getExe' (pkgs.xfce.thunar.override {thunarPlugins = osConfig.programs.thunar.plugins;}) "thunar"}"
             "$MOD, T, exec, ${lib.getExe config.programs.foot.package}"
             "$MOD, B, exec, ${lib.getExe config.programs.librewolf.package}"
             "$MOD SHIFT, P, exec, ${lib.getExe config.programs.librewolf.package} --private-window"
