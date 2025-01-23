590d90edd882c158c10fc33d8df2e3bd6d60099f
Fazzi <faaris.ansari@proton.me>
2025-01-23 13:30:27 +0000

    make a bunch of the flake inputs follow system nixpkgs. saves a ton of space and closure size smol and package size much more smol

--- flake.nix
+++ flake.nix
@@ -9,11 +9,26 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland.url = "github:hyprwm/Hyprland/";
-    apple-fonts.url = "github:Lyndeno/apple-fonts.nix";
-    nvuv.url = "gitlab:fazzi/nvuv";
-    lanzaboote.url = "github:nix-community/lanzaboote";
-    batmon.url = "github:notashelf/batmon";
-    ags.url = "github:Aylur/ags/v1"; # still on v1 lmfao
+    apple-fonts = {
+      url = "github:Lyndeno/apple-fonts.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nvuv = {
+      url = "gitlab:fazzi/nvuv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    batmon = {
+      url = "github:notashelf/batmon";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    ags = {
+      url = "github:Aylur/ags/v1"; # still on v1 lmfao
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     agenix = {
       url = "github:ryantm/agenix";
       inputs = {
