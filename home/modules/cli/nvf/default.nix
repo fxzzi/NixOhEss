414966f8a70429fa4236be757678921176568c44
Fazzi <faaris.ansari@proton.me>
2025-01-23 23:49:13 +0000

    i didn't remove the old editorconfig setup lol

--- home/modules/cli/nvf/default.nix
+++ home/modules/cli/nvf/default.nix
@@ -182,16 +182,4 @@
       };
     };
   };
-  home.file.".editorconfig".text = ''
-    root=true
-
-    [*]
-    charset=utf-8
-    end_of_line=lf
-    indent_size=2
-    indent_style=space
-    insert_final_newline=true
-    max_line_width=80
-    trim_trailing_whitespace=true
-  '';
 }
