f7da3c9afc46087e2c3ff9de5fa9fa10216582c2
Fazzi <faaris.ansari@proton.me>
2025-01-23 14:00:16 +0000

    rename ags config folder from config to ags

--- home/modules/gui/ags/default.nix
+++ home/modules/gui/ags/default.nix
@@ -9,20 +9,19 @@
     default = false;
     description = "Enables aylurs-gtk-shell and its configs.";
   };
+  # add the home manager module
   imports = [inputs.ags.homeManagerModules.default];
   config = lib.mkIf config.gui.ags.enable {
-    # add the home manager module
     programs.ags = {
       enable = true;
-
       # null or path, leave as null if you don't want hm to manage the config
       configDir = null;
     };
     xdg.configFile = {
-      "ags/icons".source = ./config/icons;
-      "ags/modules".source = ./config/modules;
-      "ags/config.js".source = ./config/config.js;
-      "ags/style.css".source = ./config/style.css;
+      "ags/icons".source = ./ags/icons;
+      "ags/modules".source = ./ags/modules;
+      "ags/config.js".source = ./ags/config.js;
+      "ags/style.css".source = ./ags/style.css;
     };
   };
 }
