f5d20d4b4e15a1a946daa553d59832fe7e00f921
Fazzi <faaris.ansari@proton.me>
2025-01-24 11:50:01 +0000

    add vrr refresh rate for mangohud

--- home/modules/apps/thunar/default.nix
+++ home/modules/apps/thunar/default.nix
@@ -39,67 +39,61 @@
       "application/x-xz" = "org.gnome.FileRoller.desktop";
       "application/x-rar-compressed" = "org.gnome.FileRoller.desktop";
     };
-    home.file = {
-      # Thunar quick actions
-      "uca.xml" = {
-        target = "${config.xdg.configHome}/Thunar/uca.xml";
-        text = ''
-          <?xml version="1.0" encoding="UTF-8"?>
-          <actions>
-          <action>
-          	<icon>clipboard</icon>
-          	<name>Copy File / Folder Path</name>
-          	<submenu></submenu>
-          	<unique-id>1653335357081852-1</unique-id>
-          	<command>echo -n &apos;&quot;%f&quot;&apos; | ${lib.getExe' pkgs.wl-clipboard "wl-copy"}</command>
-          	<description></description>
-          	<range></range>
-          	<patterns>*</patterns>
-          	<directories/>
-          	<audio-files/>
-          	<image-files/>
-          	<other-files/>
-          	<text-files/>
-          	<video-files/>
-          </action>
-          <action>
-          	<icon>foot</icon>
-          	<name>Edit in Terminal</name>
-          	<submenu></submenu>
-          	<unique-id>1715762765914315-1</unique-id>
-          	<command>${lib.getExe pkgs.foot} ${lib.getExe config.programs.nvf.finalPackage} %f</command>
-          	<description></description>
-          	<range>*</range>
-          	<patterns>*</patterns>
-          	<other-files/>
-          	<text-files/>
-          </action>
-          <action>
-          	<icon>folder-blue-script</icon>
-          	<name>Launch Terminal here</name>
-          	<submenu></submenu>
-          	<unique-id>1715763119333224-2</unique-id>
-          	<command>${lib.getExe pkgs.foot} -D %f</command>
-          	<description></description>
-          	<range>*</range>
-          	<patterns>*</patterns>
-          	<directories/>
-          </action>
-          <action>
-          	<icon>utilities-terminal_su</icon>
-          	<name>Edit as root</name>
-          	<submenu></submenu>
-          	<unique-id>1716201472079277-1</unique-id>
-          	<command>${lib.getExe pkgs.foot} ${lib.getExe' pkgs.sudo "sudoedit"} %f</command>
-          	<description></description>
-          	<range>*</range>
-          	<patterns>*</patterns>
-          	<other-files/>
-          	<text-files/>
-          </action>
-          </actions>
-        '';
-      };
-    };
+    xdg.configFile."Thunar/uca.xml".text = ''
+      <?xml version="1.0" encoding="UTF-8"?>
+      <actions>
+      <action>
+        <icon>clipboard</icon>
+        <name>Copy File / Folder Path</name>
+        <submenu></submenu>
+        <unique-id>1653335357081852-1</unique-id>
+        <command>echo -n &apos;&quot;%f&quot;&apos; | ${lib.getExe' pkgs.wl-clipboard "wl-copy"}</command>
+        <description></description>
+        <range></range>
+        <patterns>*</patterns>
+        <directories/>
+        <audio-files/>
+        <image-files/>
+        <other-files/>
+        <text-files/>
+        <video-files/>
+      </action>
+      <action>
+        <icon>foot</icon>
+        <name>Edit in Terminal</name>
+        <submenu></submenu>
+        <unique-id>1715762765914315-1</unique-id>
+        <command>${lib.getExe pkgs.foot} ${lib.getExe config.programs.nvf.finalPackage} %f</command>
+        <description></description>
+        <range>*</range>
+        <patterns>*</patterns>
+        <other-files/>
+        <text-files/>
+      </action>
+      <action>
+        <icon>folder-blue-script</icon>
+        <name>Launch Terminal here</name>
+        <submenu></submenu>
+        <unique-id>1715763119333224-2</unique-id>
+        <command>${lib.getExe pkgs.foot} -D %f</command>
+        <description></description>
+        <range>*</range>
+        <patterns>*</patterns>
+        <directories/>
+      </action>
+      <action>
+        <icon>utilities-terminal_su</icon>
+        <name>Edit as root</name>
+        <submenu></submenu>
+        <unique-id>1716201472079277-1</unique-id>
+        <command>${lib.getExe pkgs.foot} ${lib.getExe' pkgs.sudo "sudoedit"} %f</command>
+        <description></description>
+        <range>*</range>
+        <patterns>*</patterns>
+        <other-files/>
+        <text-files/>
+      </action>
+      </actions>
+    '';
   };
 }
