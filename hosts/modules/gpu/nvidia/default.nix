73381ad023862a578918ab0e96549835eb99632d
Fazzi <faaris.ansari@proton.me>
2025-02-02 14:53:12 +0000

    remove custom nvidia driver config, 570 is in nixos-unstable now, disable nvidia-settings, still kinda useless xd

--- hosts/modules/gpu/nvidia/default.nix
+++ hosts/modules/gpu/nvidia/default.nix
@@ -35,15 +35,8 @@
         open = false;
         gsp.enable = config.hardware.nvidia.open; # if using closed drivers, lets assume you don't want gsp
         powerManagement.enable = true; # Fixes nvidia-vaapi-driver after suspend
-        nvidiaSettings = true;
-        package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-          # TODO: remove this when 570 is in nixpkgs/nixos-unstable
-          version = "570.86.16"; # use new 570 drivers
-          sha256_64bit = "sha256-RWPqS7ZUJH9JEAWlfHLGdqrNlavhaR1xMyzs8lJhy9U=";
-          openSha256 = "sha256-DuVNA63+pJ8IB7Tw2gM4HbwlOh1bcDg2AN2mbEU9VPE=";
-          settingsSha256 = "sha256-9rtqh64TyhDF5fFAYiWl3oDHzKJqyOW3abpcf2iNRT8=";
-          usePersistenced = false;
-        };
+        nvidiaSettings = false;
+        package = config.boot.kernelPackages.nvidiaPackages.beta;
       };
       graphics = {
         enable = true;
