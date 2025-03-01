0bc0110cedeb53cc7fec926128d58f1d10125374
Fazzi <faaris.ansari@proton.me>
2025-03-01 10:51:04 +0000

    nix: update flakes

--- hosts/modules/gpu/nvidia/default.nix
+++ hosts/modules/gpu/nvidia/default.nix
@@ -34,11 +34,12 @@
         nvidiaSettings = false; # useless on wayland still
         # package = config.boot.kernelPackages.nvidiaPackages.beta;
         package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-          version = "570.124.04"; # use new 570 drivers
+          version = "570.124.04";
           sha256_64bit = "sha256-G3hqS3Ei18QhbFiuQAdoik93jBlsFI2RkWOBXuENU8Q=";
+          sha256_aarch64 = "sha256-HN58N00SNEwMQKSKuOMAXVW6J2VI/2YyDulQNJHuVeM=";
           openSha256 = "sha256-KCGUyu/XtmgcBqJ8NLw/iXlaqB9/exg51KFx0Ta5ip0=";
-          settingsSha256 = "";
-          usePersistenced = false;
+          settingsSha256 = "sha256-LNL0J/sYHD8vagkV1w8tb52gMtzj/F0QmJTV1cMaso8=";
+          persistencedSha256 = "sha256-SHSdnGyAiRH6e0gYMYKvlpRSH5KYlJSA1AJXPm7MDRk=";
         };
       };
       graphics = {
