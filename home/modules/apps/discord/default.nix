84098549c9f624df241bf4e5286098d70745da76
Fazzi <faaris.ansari@proton.me>
2025-03-14 20:53:51 +0000

    hm/discord: don't use discordcanary, instead use vesktop with electron 35.0.1 override, and explicit sync support. finally.

--- home/modules/apps/discord/default.nix
+++ home/modules/apps/discord/default.nix
@@ -3,7 +3,16 @@
   lib,
   config,
   ...
-}: {
+}: let
+  electronVer = "35.0.1";
+  electronPkg = pkgs.electron-bin.overrideAttrs {
+    version = electronVer;
+    src = pkgs.fetchurl {
+      url = "https://github.com/electron/electron/releases/download/v${electronVer}/electron-v${electronVer}-linux-x64.zip";
+      sha256 = "sha256-lu6Z5h7wpyeRXnY88Ypcg1Nvv3hVcCzkfU0G6XHkDQE=";
+    };
+  };
+in {
   options.cfg.apps.discord.enable = lib.mkOption {
     type = lib.types.bool;
     default = false;
@@ -11,18 +20,18 @@
   };
   config = lib.mkIf config.cfg.apps.discord.enable {
     home.packages = with pkgs; [
-      ((discord-canary.override {
-          withOpenASAR = true;
-          withVencord = true;
+      ((vesktop.override {
+          electron = electronPkg;
         })
         .overrideAttrs
         (old: {
           nativeBuildInputs = old.nativeBuildInputs or [] ++ [pkgs.makeWrapper];
-          postInstall = ''
-            ${old.postInstall or ""}
-            wrapProgramShell $out/opt/DiscordCanary/DiscordCanary \
+          postFixup = ''
+            ${old.postFixup or ""}
+            wrapProgramShell $out/bin/vesktop \
               --add-flags "--disable-smooth-scrolling" \
-              --add-flags "--disable-features=WebRtcAllowInputVolumeAdjustment"
+              --add-flags "--disable-features=WebRtcAllowInputVolumeAdjustment" \
+              --add-flags "--enable-features=WaylandLinuxDrmSyncobj" # enable explicit sync support
           '';
         }))
     ];
