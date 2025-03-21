52c329bfeca09b2f0b0fee0f08622db267efd739
Fazzi <faaris.ansari@proton.me>
2025-03-21 22:19:02 +0000

    nix/hyprland: add a few patches for testing purposes

--- hosts/modules/wayland/hyprland/default.nix
+++ hosts/modules/wayland/hyprland/default.nix
@@ -9,6 +9,16 @@
     if config.cfg.wayland.hyprland.useGit
     then inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}
     else pkgs;
+  patches =
+    if config.cfg.wayland.hyprland.useGit
+    then [
+      (pkgs.fetchpatch
+        {
+          url = "https://github.com/hyprwm/Hyprland/pull/9678.patch";
+          sha256 = "sha256-krpzC8AkzL5JyV0xXxQzG4cXazK6cP9422mnYa6c33s=";
+        })
+    ]
+    else [];
 in {
   options.cfg.wayland.hyprland.enable = lib.mkOption {
     type = lib.types.bool;
@@ -23,7 +33,9 @@ in {
   config = lib.mkIf config.cfg.wayland.hyprland.enable {
     programs.hyprland = {
       enable = true;
-      package = pkg.hyprland;
+      package = pkg.hyprland.overrideAttrs {
+        inherit patches;
+      };
       portalPackage = pkg.xdg-desktop-portal-hyprland;
       withUWSM = config.cfg.wayland.uwsm.enable;
     };
