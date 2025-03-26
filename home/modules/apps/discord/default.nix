1fd760dc9d15befc7f253e766e3f988c817f80dc
Fazzi <faaris.ansari@proton.me>
2025-03-26 10:32:41 +0000

    hm/discord: remove custom electron package, use electron_35 now in nixpkgs; nix: update flakes

--- home/modules/apps/discord/default.nix
+++ home/modules/apps/discord/default.nix
@@ -3,17 +3,7 @@
   lib,
   config,
   ...
-}: let
-  electronVer = "36.0.0-alpha.4";
-  electronPkg = pkgs.electron-bin.overrideAttrs {
-    pname = "electron_36-bin";
-    version = electronVer;
-    src = pkgs.fetchurl {
-      url = "https://github.com/electron/electron/releases/download/v${electronVer}/electron-v${electronVer}-linux-x64.zip";
-      sha256 = "sha256-3vBSlvZNEN5n7pd9gqw/rAsKgGSRiUlHLpeD64WEE8s=";
-    };
-  };
-in {
+}: {
   options.cfg.apps.discord.enable = lib.mkOption {
     type = lib.types.bool;
     default = false;
@@ -22,7 +12,7 @@ in {
   config = lib.mkIf config.cfg.apps.discord.enable {
     home.packages = with pkgs; [
       ((vesktop.override {
-          electron = electronPkg;
+          electron = electron_35;
           withTTS = false;
           withMiddleClickScroll = true;
         })
