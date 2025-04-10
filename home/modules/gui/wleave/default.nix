fbb7be9716d1ffc78847b363f645dc6166191ad7
Fazzi <faaris.ansari@proton.me>
2025-04-10 01:50:28 +0100

    hm/wleave: add neovim treesitter injection hint

--- home/modules/gui/wleave/default.nix
+++ home/modules/gui/wleave/default.nix
@@ -49,79 +49,81 @@
           }
         ];
       };
-      "wleave/style.css".text = ''
-        @import url("./colors_wleave.css");
+      "wleave/style.css".text =
+        # css
+        ''
+          @import url("./colors_wleave.css");
 
-        window {
-        	font-family: monospace;
-        	font-size: 20pt;
-        	color: #c0caf5;
-        	/* text */
-        	background-color: @background;
-        }
+          window {
+          	font-family: monospace;
+          	font-size: 20pt;
+          	color: #c0caf5;
+          	/* text */
+          	background-color: @background;
+          }
 
-        button {
-        	border-radius: 10px;
-        	background-repeat: no-repeat;
-        	background-position: center;
-        	background-size: 50%;
-        	border: none;
-        	background-color: transparent;
-        	margin: 5px;
-        	transition:
-        		box-shadow 0.1s ease-in-out,
-        		background-color 0.1s ease-in-out;
-        }
+          button {
+          	border-radius: 10px;
+          	background-repeat: no-repeat;
+          	background-position: center;
+          	background-size: 50%;
+          	border: none;
+          	background-color: transparent;
+          	margin: 5px;
+          	transition:
+          		box-shadow 0.1s ease-in-out,
+          		background-color 0.1s ease-in-out;
+          }
 
-        button:hover {
-        	background-color: @color0;
-        }
+          button:hover {
+          	background-color: @color0;
+          }
 
-        button:focus {
-        	background-color: @color1;
-        	color: @foreground;
-        }
+          button:focus {
+          	background-color: @color1;
+          	color: @foreground;
+          }
 
-        #lock {
-        	background-image: url("${./icons/lock.png}");
-        }
+          #lock {
+          	background-image: url("${./icons/lock.png}");
+          }
 
-        #lock:focus {
-        	background-image: url("${./icons/lock-hover.png}");
-        }
+          #lock:focus {
+          	background-image: url("${./icons/lock-hover.png}");
+          }
 
-        #logout {
-        	background-image: url("${./icons/logout.png}");
-        }
+          #logout {
+          	background-image: url("${./icons/logout.png}");
+          }
 
-        #logout:focus {
-        	background-image: url("${./icons/logout-hover.png}");
-        }
+          #logout:focus {
+          	background-image: url("${./icons/logout-hover.png}");
+          }
 
-        #suspend {
-        	background-image: url("${./icons/sleep.png}");
-        }
+          #suspend {
+          	background-image: url("${./icons/sleep.png}");
+          }
 
-        #suspend:focus {
-        	background-image: url("${./icons/sleep-hover.png}");
-        }
+          #suspend:focus {
+          	background-image: url("${./icons/sleep-hover.png}");
+          }
 
-        #shutdown {
-        	background-image: url("${./icons/power.png}");
-        }
+          #shutdown {
+          	background-image: url("${./icons/power.png}");
+          }
 
-        #shutdown:focus {
-        	background-image: url("${./icons/power-hover.png}");
-        }
+          #shutdown:focus {
+          	background-image: url("${./icons/power-hover.png}");
+          }
 
-        #reboot {
-        	background-image: url("${./icons/restart.png}");
-        }
+          #reboot {
+          	background-image: url("${./icons/restart.png}");
+          }
 
-        #reboot:focus {
-        	background-image: url("${./icons/restart-hover.png}");
-        }
-      '';
+          #reboot:focus {
+          	background-image: url("${./icons/restart-hover.png}");
+          }
+        '';
     };
   };
 }
