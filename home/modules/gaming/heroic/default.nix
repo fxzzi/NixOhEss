f0d3e1dc0efd59ab9af6f19a94b865e7379fb22d
Fazzi <faaris.ansari@proton.me>
2025-04-10 15:18:16 +0100

    hm/heroic: wrap heroic with flag to force x11, fixes the ui not launching

--- home/modules/gaming/heroic/default.nix
+++ home/modules/gaming/heroic/default.nix
@@ -3,14 +3,23 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  heroic = pkgs.symlinkJoin {
+    name = "heroic-wrapped";
+    paths = [pkgs.heroic];
+    nativeBuildInputs = [pkgs.makeWrapper];
+    postBuild = ''
+      wrapProgram $out/bin/heroic --add-flags "--ozone-platform=x11"
+    '';
+  };
+in {
   options.cfg.gaming.heroic.enable = lib.mkOption {
     type = lib.types.bool;
     default = false;
     description = "Enables the Heroic Games Launcher";
   };
   config = lib.mkIf config.cfg.gaming.heroic.enable {
-    home.packages = with pkgs; [
+    home.packages = [
       heroic
     ];
     xdg = {
