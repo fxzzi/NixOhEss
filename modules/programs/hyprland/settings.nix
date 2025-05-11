2724e567f5212cb4c2bfef125beea67e7b55929d
Fazzi <faaris.ansari@proton.me>
2025-05-11 01:12:17 +0100

    scripts: remove screenshot script since i have it in hyprland conf anyway

--- modules/programs/hyprland/settings.nix
+++ modules/programs/hyprland/settings.nix
@@ -68,7 +68,9 @@
       fileName="Screenshot from $(date '+%y.%m.%d %H:%M:%S').png"
       screenshotDir="$HOME/Pictures/Screenshots"
       path="$screenshotDir/$fileName"
+      grimCmd="grim -t png -l 1"
 
+      # make the screenshot directory if it doesn't already exist
       if [ ! -d "$screenshotDir" ]; then
         mkdir -p "$screenshotDir"
         echo "Directory '$screenshotDir' created successfully."
@@ -77,33 +79,30 @@
       case $1 in
       --monitor)
         if [ -z "$2" ]; then
-          echo "Error: Monitor output not specified."
+          echo "give monitor output too"
           exit 1
         fi
-        grim -t png -l 1 -o "$2" "$path"
+         $grimCmd -o "$2" "$path"
         ;;
       --selection)
         wayfreeze --hide-cursor &
         PID=$!
         sleep .1
-        grim -t png -l 1 -g "$(slurp)" "$path"
+        $grimCmd -g "$(slurp)" "$path"
         kill $PID
         ;;
       --active)
         window_geometry=$(hyprctl activewindow -j | jq -r '"\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"')
 
-        grim -t png -l 1 -g "$window_geometry" "$path"
-        ;;
-      *)
-        echo "invalid cmd"
-        exit 1
+        $grimCmd -g "$window_geometry" "$path"
         ;;
       esac
 
+      # if the screenshot was not cancelled
       if [ -f "$path" ]; then
-        canberra-gtk-play -i camera-shutter -V "-10" &
+        canberra-gtk-play -i camera-shutter & # play shutter sound
         dunstify -i "$path" -a "screenshot" "Screenshot" "Copied to clipboard" -r 9998 &
-        wl-copy <"$path"
+        wl-copy < "$path" # copy to clipboard
         exit 0
       fi
 
@@ -139,7 +138,7 @@ in {
           "${runProc "${pkgs.mate.mate-polkit}/etc/xdg/autostart/polkit-mate-authentication-agent-1.desktop"}"
         ];
         exec = [
-          "sleep 0.5; ${runOnce inputs.ags.packages.${pkgs.system}.default}"
+          "${runOnce inputs.ags.packages.${pkgs.system}.default}"
           "${runProc "${lib.getExe pkgs.xorg.xrandr} --output ${config.cfg.gui.hypr.defaultMonitor} --primary"}"
         ];
         monitor = [
