9df4dac89b5a8ba8287302f5163f78bed97181f1
Fazzi <faaris.ansari@proton.me>
2025-05-14 18:34:07 +0100

    nix: update flakes

--- modules/programs/music.nix
+++ modules/programs/music.nix
@@ -4,7 +4,7 @@
   config,
   ...
 }: {
-  options.cfg.music.extraApps.enable = lib.mkEnableOption "extraApps";
+  options.cfg.music.extraApps.enable = lib.mkEnableOption "extra music apps";
   config = lib.mkIf config.cfg.music.extraApps.enable {
     hj = {
       packages = with pkgs; [
