76d27dd9364291019c123579e6b275e8931c054a
Fazzi <faaris.ansari@proton.me>
2025-05-18 15:50:36 +0100

    vesktop: fixup quickcss

--- modules/programs/vesktop.nix
+++ modules/programs/vesktop.nix
@@ -51,9 +51,18 @@ in {
           /* Hide the Visual Refresh title bar */
           @import url("https://raw.githubusercontent.com/surgedevs/visual-refresh-compact-title-bar/refs/heads/main/hidden.css");
 
+          /* Use system fonts for UI */
+          :root {
+            --font-primary: ${primaryFont} !important;
+            --font-display: ${primaryFont} !important;
+            --font-headline: ${primaryFont} !important;
+            --font-code: ${monoFont} !important;
+            /* Make the sidebar line up correctly with disabled title bar */
+            --vr-header-snippet-server-padding: 8px !important;
+          }
+
           /* Align the chat box with the user panel */
-          form>div[class^="channelBottomBarArea_"],
-          form>div>div[class^="channelBottomBarArea_"] {
+          form div[class^="channelBottomBarArea_"] {
             --custom-chat-input-margin-bottom: 6px;
             --custom-channel-textarea-text-area-height: 52px;
           }
@@ -68,16 +77,6 @@ in {
             display: none;
           }
 
-          /* Use system fonts for UI */
-          :root {
-            --font-primary: ${primaryFont} !important;
-            --font-display: ${primaryFont} !important;
-            --font-headline: ${primaryFont} !important;
-            --font-code: ${monoFont} !important;
-            /* Make the sidebar line up correctly with disabled title bar */
-            --vr-header-snippet-server-padding: 8px !important;
-          }
-
           /* Hide "Now Playing" section on friends list */
           div[class^="nowPlayingColumn_"] {
             display: none !important;
@@ -88,46 +87,6 @@ in {
             font-size: 11px;
             font-weight: normal;
           }
-
-          /* Hide "Discover" icon in channel list */
-          div[class="circleIconButton__5bc7e discoveryIcon_ef3116"] {
-            display: none !important;
-          }
-
-          /* reduce context menu padding */
-          .scroller_c1e9c4{
-              margin: -4px;
-              &::after,.separator_c1e9c4 {
-                  margin: 1px 8px !important;
-              }
-              &::after,.wrapper_f563df {
-                  padding: 2px !important;
-              }
-              &::after,.item_c1e9c4{
-                  /* min-height is 32px as default, change value for desired look */
-                  min-height: 32;
-                  /* padding is 4px 8px as default, change value for desired look */
-                  padding: 4px 8px;
-              }
-          }
-          /* Chatbox buttons */
-          form>div>div[class^=channelBottomBarArea_] {
-            div[class^="buttons_"]
-            {
-              /* Send gift button (both versions) */
-              > div:has(div[class*="trinketsIcon_"]), > button { display: none; }
-
-              /* GIF picker button */
-              div[class^="expression-picker-chat-input-button"]:has( + div[class^="expression-picker-chat-input-button"] div[class*="stickerButton_"]) { display: var(--display-gif-button, flex); }
-
-              /* Sticker picker button */
-              div[class^="expression-picker-chat-input-button"]:has(div[class*="stickerButton_"]) { display: var(--display-sticker-button, flex); }
-            }
-
-            /* Apps button (right) */
-            div[class^="channelAppLauncher_"] { display: var(--display-app-launcher); }
-          }
-
         '';
 
       packages = with pkgs; [
