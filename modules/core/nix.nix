5f903ddb23047b34ff79db253d81ebebca51c203
Fazzi <faaris.ansari@proton.me>
2025-05-26 16:39:19 +0100

    nix: set TMPDIR for daemon to avoid building stuff on tmpfs, as it can easily run out of space

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -12,10 +12,16 @@
         use-xdg-base-directories = true; # clean up ~
         allowed-users = ["@wheel"];
         trusted-users = ["@wheel"];
+        build-dir = "/var/tmp";
       };
     };
     nixpkgs.config.allowUnfree = true; # not too fussed as long as app works on linux tbh
     documentation.nixos.enable = false; # remove useless docs .desktop
     system.rebuild.enableNg = true; # use python based nixos-rebuild
+
+    # don't build stuff on tmpfs, it can easily run out of space
+    systemd.services.nix-daemon = {
+      environment.TMPDIR = "/var/tmp";
+    };
   };
 }
