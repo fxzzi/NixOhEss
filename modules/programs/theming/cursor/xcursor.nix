335a87313de147e8928a19b8d59bc969a5fbc941
Fazzi <faaris.ansari@proton.me>
2025-06-03 01:19:02 +0100

    dconf: lockAll to ensure its fully declaritive

--- modules/programs/theming/cursor/xcursor.nix
+++ modules/programs/theming/cursor/xcursor.nix
@@ -47,24 +47,14 @@ in {
     };
     programs.dconf.profiles.user.databases = [
       {
-        settings."org/gnome/desktop/interface" = {
-          cursor-theme = xcursor;
-          cursor-size = lib.gvariant.mkInt32 24;
+        lockAll = true;
+        settings = {
+          "org/gnome/desktop/interface" = {
+            cursor-theme = xcursor;
+            cursor-size = lib.gvariant.mkInt32 24;
+          };
         };
       }
     ];
-    # systemd.user.services.dconf-xcursor = {
-    #   enable = true;
-    #   unitConfig = {
-    #     ConditionEnvironment = "WAYLAND_DISPLAY";
-    #     Description = "Set dconf cursor settings";
-    #     After = ["graphical-session-pre.target"];
-    #     PartOf = ["graphical-session.target"];
-    #   };
-    #   script = ''
-    #     ${dconf} write /org/gnome/desktop/interface/cursor-theme \"'${xcursor}'\"
-    #     ${dconf} write /org/gnome/desktop/interface/cursor-size \"'24'\"
-    #   '';
-    # };
   };
 }
