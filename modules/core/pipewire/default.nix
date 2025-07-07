6ec4613e9138152f203d9a8699ae00bed7870cb3
Fazzi <faaris.ansari@proton.me>
2025-07-07 11:17:51 +0100

    pipewire: configure quantum; fazziPC: don't blank between TTY and Hyprland

--- modules/core/pipewire/default.nix
+++ modules/core/pipewire/default.nix
@@ -10,6 +10,39 @@
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
+      extraConfig.pipewire."92-quantum" = {
+        "context.properties" = {
+          "default.clock.rate" = 48000;
+          "default.clock.quantum" = 128;
+          "default.clock.min-quantum" = 32;
+          "default.clock.max-quantum" = 512;
+        };
+      };
+      extraConfig.pipewire-pulse."92-quantum" = {
+        context.modules = [
+          {
+            name = "libpipewire-module-rtkit";
+            args = {
+              "nice.level" = -20;
+              "rt.prio" = 99;
+            };
+          }
+          {
+            name = "libpipewire-module-protocol-pulse";
+            args.pulse = {
+              min.req = "32/48000";
+              default.req = "128/48000";
+              max.req = "512/48000";
+              min.quantum = "32/48000";
+              max.quantum = "512/48000";
+            };
+          }
+        ];
+        stream.properties = {
+          node.latency = "128/48000";
+          resample.quality = 1;
+        };
+      };
     };
 
     # Optional but recommended
