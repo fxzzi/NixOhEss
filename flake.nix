b55c8b134079175cbacc977c25aaf9e113c75a33
Fazzi <faaris.ansari@proton.me>
2025-07-25 12:41:57 +0500

    flake.nix: cleanup, move inputs to bottom, move hyprland deps to bottom

--- flake.nix
+++ flake.nix
@@ -1,6 +1,46 @@
 {
   description = "fazzi's nixos + hjem conf";
 
+  outputs = inputs: let
+    npins = import ./npins;
+    xLib = import ./lib inputs.nixpkgs.lib;
+
+    nixosCommonSystem = {
+      hostName,
+      user,
+    }:
+      inputs.nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit
+            inputs
+            npins
+            hostName
+            user
+            xLib
+            ;
+        };
+        modules = [
+          ./modules
+          ./hosts/${hostName}
+        ];
+      };
+
+    # adding a host? Add a hostname and username here
+    # then, have a gander at ./hosts
+    hosts = {
+      fazziPC.user = "faaris";
+      fazziGO.user = "faaris";
+      kunzozPC.user = "kunzoz";
+    };
+  in {
+    nixosConfigurations =
+      builtins.mapAttrs (
+        hostName: config:
+          nixosCommonSystem (config // {inherit hostName;})
+      )
+      hosts;
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/x86_64-linux";
@@ -11,6 +51,71 @@
         smfh.follows = ""; # we use smfh from nixpkgs
       };
     };
+
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+        aquamarine.follows = "aquamarine";
+        hyprcursor.follows = "hyprcursor";
+        hyprgraphics.follows = "hyprgraphics";
+        hyprland-protocols.follows = "hyprland-protocols";
+        hyprland-qtutils.follows = "hyprland-qtutils";
+        hyprlang.follows = "hyprlang";
+        hyprutils.follows = "hyprutils";
+        hyprwayland-scanner.follows = "hyprwayland-scanner";
+        xdph.follows = "xdph";
+      };
+    };
+    ags = {
+      url = "github:aylur/ags/v1";
+      # url = "github:NotAShelf/rags"; # raf's agsv1 fork
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+      };
+    };
+    nvuv = {
+      url = "gitlab:fazzi/nvuv";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+      };
+    };
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    watt = {
+      url = "github:NotAShelf/watt";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = ""; # ew, home-manager
+        darwin.follows = ""; # ew, apple
+      };
+    };
+    nvf = {
+      url = "github:NotAShelf/nvf";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+      };
+    };
+    creamlinux = {
+      # FIXME: pin to older release until i have the time to update the flake
+      url = "github:Novattz/creamlinux-installer/17ad517a459f1a41a40bef2642ee952859147ab5";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+      };
+    };
+
+    # hyprland deps
     aquamarine = {
       url = "github:hyprwm/aquamarine";
       inputs = {
@@ -92,109 +197,8 @@
         hyprwayland-scanner.follows = "hyprwayland-scanner";
       };
     };
-    hyprland = {
-      url = "github:hyprwm/Hyprland";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        systems.follows = "systems";
-        aquamarine.follows = "aquamarine";
-        hyprcursor.follows = "hyprcursor";
-        hyprgraphics.follows = "hyprgraphics";
-        hyprland-protocols.follows = "hyprland-protocols";
-        hyprland-qtutils.follows = "hyprland-qtutils";
-        hyprlang.follows = "hyprlang";
-        hyprutils.follows = "hyprutils";
-        hyprwayland-scanner.follows = "hyprwayland-scanner";
-        xdph.follows = "xdph";
-      };
-    };
-    ags = {
-      url = "github:aylur/ags/v1";
-      # url = "github:NotAShelf/rags"; # raf's agsv1 fork
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        systems.follows = "systems";
-      };
-    };
-    nvuv = {
-      url = "gitlab:fazzi/nvuv";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        systems.follows = "systems";
-      };
-    };
-    lanzaboote = {
-      url = "github:nix-community/lanzaboote";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    watt = {
-      url = "github:NotAShelf/watt";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    agenix = {
-      url = "github:ryantm/agenix";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        home-manager.follows = ""; # ew, home-manager
-        darwin.follows = ""; # ew, apple
-      };
-    };
-    nvf = {
-      url = "github:NotAShelf/nvf";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        systems.follows = "systems";
-      };
-    };
-    creamlinux = {
-      # FIXME: pin to older release until i have the time to update the flake
-      url = "github:Novattz/creamlinux-installer/17ad517a459f1a41a40bef2642ee952859147ab5";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        systems.follows = "systems";
-      };
-    };
   };
 
-  outputs = inputs: let
-    npins = import ./npins;
-    xLib = import ./lib inputs.nixpkgs.lib;
-
-    nixosCommonSystem = {
-      hostName,
-      user,
-    }:
-      inputs.nixpkgs.lib.nixosSystem {
-        specialArgs = {
-          inherit
-            inputs
-            npins
-            hostName
-            user
-            xLib
-            ;
-        };
-        modules = [
-          ./modules
-          ./hosts/${hostName}
-        ];
-      };
-
-    # adding a host? Add a hostname and username here
-    # then, have a gander at ./hosts
-    hosts = {
-      fazziPC = {user = "faaris";};
-      fazziGO = {user = "faaris";};
-      kunzozPC = {user = "kunzoz";};
-    };
-  in {
-    nixosConfigurations =
-      builtins.mapAttrs (
-        hostName: config:
-          nixosCommonSystem (config // {inherit hostName;})
-      )
-      hosts;
-  };
   nixConfig = {
     extra-substituters = [
       "https://hyprland.cachix.org"
