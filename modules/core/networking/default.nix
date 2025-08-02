a4e83900c4eca0a181b6d911e9902c2759e478e6
fazzi <faaris.ansari@proton.me>
2025-08-02 15:02:20 +0100

    networking: also add cloudflare ipv6 nameservers

--- modules/core/networking/default.nix
+++ modules/core/networking/default.nix
@@ -16,6 +16,8 @@
       nameservers = [
         "1.1.1.1#one.one.one.one"
         "1.0.0.1#one.one.one.one"
+        "2606:4700:4700::1111#one.one.one.one"
+        "2606:4700:4700::1001#one.one.one.one"
       ];
 
       firewall = {
