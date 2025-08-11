b2bc4dabb6e2641c570ad0e5cd507dee2520a10b
fazzi <faaris.ansari@proton.me>
2025-08-11 18:17:46 +0100

    nix: update flakes
    zenergy doesn't yet support kernel 6.16 without an unmerged PR. So add
    it as a patch.

--- modules/core/kernel/zenergy.nix
+++ modules/core/kernel/zenergy.nix
@@ -1,16 +1,32 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: let
   inherit (lib) mkEnableOption mkIf;
+  inherit (pkgs) fetchpatch;
   cfg = config.cfg.core.kernel.zenergy;
 in {
   options.cfg.core.kernel.zenergy.enable = mkEnableOption "zenergy";
   config = mkIf cfg.enable {
     boot = {
       kernelModules = ["nct6775"]; # provides temp readings
-      extraModulePackages = with config.boot.kernelPackages; [zenergy]; # provides power readings
+      # extraModulePackages = with config.boot.kernelPackages; [zenergy]; # provides power readings
+      # FIXME: remove when zenergy is updated to support 6.16
+      extraModulePackages = [
+        (config.boot.kernelPackages.zenergy.overrideAttrs
+          (old: {
+            patches =
+              (old.patches or [])
+              ++ [
+                (fetchpatch {
+                  url = "https://github.com/BoukeHaarsma23/zenergy/pull/18.patch";
+                  hash = "sha256-Xe6CaiU9Vcxxgzu4VGrriq5sAQfymFbsw4kEf3Gj2C0=";
+                })
+              ];
+          }))
+      ];
     };
   };
 }
