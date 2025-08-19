a00e63ff426b035f79e6806ac79939ef28269bb6
fazzi <faaris.ansari@proton.me>
2025-08-19 19:34:59 +0100

    greetd: enable new useTextGreeter option, remove extra sysd config

--- modules/services/greetd.nix
+++ modules/services/greetd.nix
@@ -15,6 +15,7 @@ in {
   config = mkIf cfg.enable {
     services.greetd = {
       enable = true;
+      useTextGreeter = true;
       settings = {
         default_session = {
           command =
@@ -32,20 +33,6 @@ in {
           user = "greeter";
         };
       };
-    };
-    # this is a life saver.
-    # literally no documentation about this anywhere.
-    # might be good to write about this...
-    # https://www.old.reddit.com/r/NixOS/comments/u0cdpi/tuigreet_with_xmonad_how/
-    systemd.services.greetd.serviceConfig = {
-      Type = "idle";
-      StandardInput = "tty";
-      StandardOutput = "tty";
-      StandardError = "journal"; # Without this errors will spam on screen
-      # Without these bootlogs will spam on screen
-      TTYReset = true;
-      TTYVHangup = true;
-      TTYVTDisallocate = true;
     };
   };
 }
