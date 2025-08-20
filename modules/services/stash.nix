c193115963442ffb5576bd52450d0e25038686e4
fazzi <faaris.ansari@proton.me>
2025-08-20 12:06:58 +0100

    stash: add filtering regex

--- modules/services/stash.nix
+++ modules/services/stash.nix
@@ -6,11 +6,12 @@
   ...
 }: let
   inherit (lib) mkEnableOption mkIf getExe;
-  inherit (pkgs) callPackage;
+  inherit (pkgs) callPackage writeText;
   cfg = config.cfg.services.stash;
   stash = callPackage "${npins.stash}/nix/package.nix" {
     craneLib = callPackage "${npins.crane}/lib" {};
   };
+  regex = "(password|secret|api[_-]?key|token)[=: ]+[^\s]+";
 in {
   options.cfg.services.stash.enable = mkEnableOption "stash" // {default = true;};
   config = mkIf cfg.enable {
@@ -22,6 +23,7 @@ in {
       partOf = ["graphical-session.target"];
       unitConfig = {
         ConditionEnvironment = "WAYLAND_DISPLAY"; # Only start if WAYLAND_DISPLAY env var is set
+        loadCredential = "clipboard_filter:${writeText "stash-regex" regex}";
       };
       serviceConfig = {
         ExecStart = "${getExe stash} --max-items 10 watch";
