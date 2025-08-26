fb83349b95ee0e96edeb0a427d2c1aea230f9373
fazzi <faaris.ansari@proton.me>
2025-08-26 12:21:40 +0100

    zsh: switch to fast-syntax-highlighting, remove some redundant options

--- modules/programs/zsh.nix
+++ modules/programs/zsh.nix
@@ -19,7 +19,7 @@ in {
         bat
         eza
       ];
-      xdg.config.files."zsh/.zshrc".text = mkBefore "# this is a comment :P";
+      xdg.config.files."zsh/.zshrc".text = mkBefore "# i am not a new zsh user...";
     };
     # de-clutter $HOME
     environment.sessionVariables = {
@@ -36,7 +36,6 @@ in {
           # history options to ignore dups and stuff
           "EXTENDED_HISTORY"
           "HIST_EXPIRE_DUPS_FIRST"
-          "HIST_IGNORE_DUPS"
           "HIST_IGNORE_ALL_DUPS"
           "HIST_FIND_NO_DUPS"
           "HIST_SAVE_NO_DUPS"
@@ -77,9 +76,8 @@ in {
             # tells zsh to ignore case when completing commands or filenames.
             zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
-            # Select the Bash word style so backward-kill-word goes to last / or .
-            autoload -U select-word-style
-            select-word-style bash
+            # ctrl-left/right and ctrl-bspc will limit here
+            WORDCHARS='*?_-.[]~=&;!$%^(){}<>'
 
             # `paste` command which allows you to upload text to a pastebin
             # usage: paste <file> or <command> | paste
@@ -91,15 +89,17 @@ in {
             }
 
             # plugins and completions
+            zstyle ':fzf-tab:complete:cd:*' fzf-preview '${config.programs.zsh.shellAliases.ls} --color=always --width 1 $realpath'
             fpath+=${pkgs.zsh-completions}/share/zsh/site-functions
             source ${pkgs.nix-zsh-completions}/share/zsh/plugins/nix/nix-zsh-completions.plugin.zsh
             source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
             source ${pkgs.zsh-fzf-history-search}/share/zsh-fzf-history-search/zsh-fzf-history-search.plugin.zsh
             source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
             source ${pkgs.zsh-history-substring-search}/share/zsh-history-substring-search/zsh-history-substring-search.zsh
-            source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+            # source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+            source ${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh
 
-            # these keybinds have to be set after the plugins
+            # these keybinds have to be set after the plugin is sourced
             bindkey "^[[A" history-substring-search-up
             bindkey "^[[B" history-substring-substring-search-down
             bindkey "^[OA" history-substring-search-up
