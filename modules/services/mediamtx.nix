6fc231b15c79a213f00007b7e283849e91b7e509
fazzi <faaris.ansari@proton.me>
2025-08-30 17:39:27 +0100

    mediamtx: remove all webrtcAdditionalHosts
    will add back if required. some minor testing indicated i actually
    didn't need the dns server ip's or local ip's. So now just use the
    secret for public ip.

--- modules/services/mediamtx.nix
+++ modules/services/mediamtx.nix
@@ -8,9 +8,6 @@
   inherit (builtins) concatLists map attrValues;
   cfg = config.cfg.services.mediamtx;
   port = "4200";
-  # get all assigned local ips
-  localips = concatLists (map (iface: map (addr: addr.address) iface.ipv4.addresses) (attrValues config.networking.interfaces));
-  # mediamtx doesnt support ipv6, and it fails to work if there is one present. so filter for ipv4 only
 in {
   options.cfg.services.mediamtx.enable = mkEnableOption "mediamtx";
   config = mkIf cfg.enable {
@@ -29,12 +26,6 @@ in {
         webrtc = true;
         webrtcAddress = ":${port}";
         webrtcLocalUDPAddress = ":${port}";
-        webrtcAdditionalHosts =
-          [
-            "1.1.1.1"
-            "1.0.0.1"
-          ]
-          ++ localips;
         # allow publishing to all paths
         paths = {
           all_others = {};
