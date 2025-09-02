c1e8f04ad6af644af3f858f609af6557f2fee168
fazzi <faaris.ansari@proton.me>
2025-09-02 11:36:09 +0100

    zram: set max to 8gb
    also remove a few of the extra systctl settings, it made my ram usage
    about a gigabyte higher just straight off the the bat. Also make
    swappiness slightly less aggressive.

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -55,6 +55,8 @@ in {
     zramSwap = {
       enable = true;
       memoryPercent = 50;
+      # in bytes
+      memoryMax = 8 * 1024 * 1024 * 1024;
     };
     boot = {
       initrd.systemd.enable = true;
@@ -80,13 +82,8 @@ in {
       extraModprobeConfig = ''
         blacklist sp5100_tco
       '';
-      # NOTE: https://wiki.archlinux.org/title/Zram#Optimizing_swap_on_zram
-      kernel.sysctl = {
-        "vm.swappiness" = 180;
-        "vm.watermark_boost_factor" = 0;
-        "vm.watermark_scale_factor" = 125;
-        "vm.page-cluster" = 0;
-      };
+      # zram is fast enough that we can be aggressive with swappiness
+      kernel.sysctl."vm.swappiness" = 130;
     };
   };
 }
