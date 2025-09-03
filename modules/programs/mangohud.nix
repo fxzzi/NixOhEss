1ea41df251b6b2385ab7d6260fcdf595b6e1b49f
fazzi <faaris.ansari@proton.me>
2025-09-03 15:34:20 +0100

    mangohud: override to latest git
    fixes a few issues, including keybinds not working in quite a few cases
    with wayland native games / wine, and fixes the overlay showing on all
    gtk4 apps. Also stops mangohud from showing on mpv.

--- modules/programs/mangohud.nix
+++ modules/programs/mangohud.nix
@@ -2,31 +2,12 @@
   config,
   lib,
   pkgs,
+  npins,
   ...
 }: let
-  inherit (lib) mkEnableOption mkOption types mkIf concatStringsSep lists attrsets strings isBool;
-  inherit (builtins) toString typeOf;
+  inherit (lib) mkEnableOption mkOption types mkIf;
+  inherit (builtins) substring toString;
   cfg = config.cfg.programs.mangohud;
-
-  renderOption = option:
-    rec {
-      int = toString option;
-      float = int;
-      path = int;
-      bool = "0"; # "on/off" opts are disabled with `=0`
-      string = option;
-      list = concatStringsSep "," (lists.forEach option toString);
-    }
-    .${
-      typeOf option
-    };
-
-  renderLine = k: v: (
-    if isBool v && v
-    then k
-    else "${k}=${renderOption v}"
-  );
-  renderSettings = attrs: strings.concatStringsSep "\n" (attrsets.mapAttrsToList renderLine attrs) + "\n";
 
   fpsLimit = let
     rr = cfg.refreshRate;
@@ -35,6 +16,22 @@
     then rr
     # NOTE:https://old.reddit.com/r/nvidia/comments/1lokih2/putting_misconceptions_about_optimal_fps_caps/
     else (rr - (rr * rr / 4096));
+
+  # override MangoHud to latest git until next release.
+  # fixes keybinds on native wayland games, and avoids mangohud
+  # showing up on all gtk4 apps.
+  mangohud = pkgs.mangohud.overrideAttrs {
+    version = "0-unstable-${substring 0 8 npins.MangoHud.revision}";
+    src = npins.MangoHud;
+  };
+  package = mangohud.override {
+    mangohud32 = pkgs.pkgsi686Linux.mangohud.overrideAttrs {
+      version = "0-unstable-${substring 0 8 npins.MangoHud.revision}";
+      src = npins.MangoHud;
+    };
+    gamescopeSupport = config.cfg.programs.gamescope.enable;
+    nvidiaSupport = config.cfg.hardware.nvidia.enable;
+  };
 in {
   options.cfg.programs.mangohud = {
     enable = mkEnableOption "mangohud";
@@ -57,21 +54,19 @@ in {
       "MANGOHUD" = "1";
     };
     hj = {
-      packages = [pkgs.mangohud];
+      packages = [package];
       xdg.config.files = {
-        "MangoHud/MangoHud.conf".text = renderSettings {
-          preset = "0,1,2";
-          fps_limit = "${toString fpsLimit},0"; # few below refresh rate (vrr) or unlimited
-          toggle_hud = "Shift_R+F12";
-          toggle_hud_position = "Shift_R+F11";
-          toggle_preset = "Shift_R+F10";
-          font_size = 19;
-          vsync = 1; # disable vulkan vsync
-          gl_vsync = 0; # disable opengl vsync
-          blacklist = [
-            "mpv"
-          ];
-        };
+        "MangoHud/MangoHud.conf".text = ''
+          blacklist=mpv
+          font_size=19
+          fps_limit=${toString fpsLimit},0
+          gl_vsync=0
+          preset=0,1,2
+          toggle_hud=Shift_R+F12
+          toggle_hud_position=Shift_R+F11
+          toggle_preset=Shift_R+F10
+          vsync=1
+        '';
         "MangoHud/presets.conf".text = ''
           [preset 0]
           fps_only
