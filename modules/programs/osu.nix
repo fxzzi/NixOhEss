e954f6ad2adadcb53c7c45b102d551da188cca5a
Fazzi <faaris.ansari@proton.me>
2025-10-21 20:25:57 +0100

    kunzozPC: enable hyprsunset

--- modules/programs/osu.nix
+++ modules/programs/osu.nix
@@ -19,7 +19,7 @@
     gmrun_enable = false;
     # HACK: adding env vars like so needs `env` as you can't add
     # env vars after an `exec` command.
-    command_prefix = "env OSU_SDL3=1";
+    # command_prefix = "env OSU_SDL3=1";
     # releaseStream = "tachyon";
   };
 in {
