f8b16e28de43a6afdcc150bf3164802a1768b6bb
Fazzi <faaris.ansari@proton.me>
2025-10-31 17:42:59 +0000

    flake-parts: move packages to pkgs

--- parts/fmt.nix
+++ parts/fmt.nix
@@ -1,6 +1,17 @@
 _: {
-  perSystem = {config, ...}: {
-    # alejandra and co. for formatting
-    formatter = config.packages.alejFmt;
+  perSystem = {pkgs, ...}: {
+    formatter = pkgs.writeShellApplication {
+      name = "alejFmt";
+      runtimeInputs = with pkgs; [
+        alejandra
+        deadnix
+        statix
+        fd
+      ];
+      text = ''
+        fd "$@" -t f -e nix -x statix fix -- '{}'
+        fd "$@" -t f -e nix -X deadnix -e -- '{}' \; -X alejandra -q '{}'
+      '';
+    };
   };
 }
