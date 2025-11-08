603d6aa5e09a1ef63d53ad449862b5dbf529b40b
Fazzi <faaris.ansari@proton.me>
2025-11-08 01:21:01 +0000

    amdgpu: check for kernel ver at least 6.17 before applying patch

--- modules/hardware/amdgpu.nix
+++ modules/hardware/amdgpu.nix
@@ -10,16 +10,19 @@
 in {
   options.cfg.hardware.amdgpu.enable = mkEnableOption "amdgpu";
   config = mkIf cfg.enable {
-    # early load / early kms
-    hardware.amdgpu.initrd.enable = true;
-    boot.extraModulePackages = [
+    # only used patched amdgpu on kernels >= 6.17
+    boot.extraModulePackages = mkIf (config.boot.kernelPackages.kernelAtLeast "6.17") [
       (self.packages.${pkgs.stdenv.hostPlatform.system}.amdgpu-kernel-module.override {
         inherit (config.boot.kernelPackages) kernel;
       })
     ];
-    hardware.graphics = {
-      enable = true;
-      enable32Bit = true;
+    hardware = {
+      # early load / early kms
+      amdgpu.initrd.enable = true;
+      graphics = {
+        enable = true;
+        enable32Bit = true;
+      };
     };
   };
 }
