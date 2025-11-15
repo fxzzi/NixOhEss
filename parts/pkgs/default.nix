46ca541a73582c50f26e1f034202135d06d1a223
Fazzi <faaris.ansari@proton.me>
2025-11-15 17:41:11 +0000

    flake-parts: steal some comments from notashelf/nyx

--- parts/pkgs/default.nix
+++ parts/pkgs/default.nix
@@ -8,11 +8,26 @@ in {
   perSystem = {
     pkgs,
     system,
+    config,
     ...
   }: {
-    _module.args.pkgs = import inputs.nixpkgs {
+    # Configure nixpkgs locally and expose it as <flakeRef>.legacyPackages.
+    # This will then be consumed to override flake-parts' pkgs argument
+    # to make sure pkgs instances in flake-parts modules are all referring
+    # to the same configuration instance - this one.
+    legacyPackages = import inputs.nixpkgs {
       inherit system;
-      config.allowUnfree = true;
+      config = {
+        allowUnfree = true;
+      };
+    };
+    _module.args = {
+      # Unify all instances of nixpkgs into a single `pkgs` set
+      # Wthat includes our own overlays within `perSystem`. This
+      # is not done by flake-parts, so we do it ourselves.
+      # See:
+      #  <https://github.com/hercules-ci/flake-parts/issues/106#issuecomment-1399041045>
+      pkgs = config.legacyPackages;
     };
     packages =
       # some of our pkgs depend on each other, so use fix and pass self through
