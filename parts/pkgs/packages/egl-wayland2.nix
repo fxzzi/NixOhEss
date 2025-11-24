d789e778a8402bca1fd3e9a3c32e5b49fa2b5e25
Fazzi <faaris.ansari@proton.me>
2025-11-24 21:56:49 +0000

    pkgs/egl-wayland2: take precedence over v1, use fork

--- parts/pkgs/packages/egl-wayland2.nix
+++ parts/pkgs/packages/egl-wayland2.nix
@@ -38,8 +38,12 @@ stdenv.mkDerivation {
     eglexternalplatform
   ];
 
-  postFixup = ''
-    substituteInPlace "$out/share/egl/egl_external_platform.d/09_nvidia_wayland2.json" \
+  fixupPhase = ''
+    # driver is currently broken, lower number should in theory take precedence
+    # however it doesn't. 11 > 10 from egl-wayland1
+    mv $out/share/egl/egl_external_platform.d/09_nvidia_wayland2.json \
+      $out/share/egl/egl_external_platform.d/11_nvidia_wayland2.json
+    substituteInPlace "$out/share/egl/egl_external_platform.d/11_nvidia_wayland2.json" \
       --replace '"libnvidia-egl-wayland2.so.1"' "\"$out/lib/libnvidia-egl-wayland2.so.1\""
   '';
 
