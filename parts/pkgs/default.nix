07b1f4c391e2227cab6b96f6964339dd1f0d87c5
Fazzi <faaris.ansari@proton.me>
2025-11-27 12:40:45 +0000

    pkgs/stremio-linux-shell: add patches and use opengl

--- parts/pkgs/default.nix
+++ parts/pkgs/default.nix
@@ -9,24 +9,17 @@ in {
   perSystem = {
     pkgs,
     system,
-    config,
     ...
   }: {
-    # Configure nixpkgs locally and expose it as <flakeRef>.legacyPackages.
-    # This will then be consumed to override flake-parts' pkgs argument
-    # to make sure pkgs instances in flake-parts modules are all referring
-    # to the same configuration instance - this one.
-    legacyPackages = import inputs.nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    };
     _module.args = {
       # Unify all instances of nixpkgs into a single `pkgs` set
-      # that includes our own overlays within `perSystem`. This
-      # is not done by flake-parts, so we do it ourselves.
+      # this is not done by flake-parts, so we do it ourselves.
       # See:
       #  <https://github.com/hercules-ci/flake-parts/issues/106#issuecomment-1399041045>
-      pkgs = config.legacyPackages;
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
     };
     packages =
       # some of our pkgs depend on each other, so use fix and pass self through
