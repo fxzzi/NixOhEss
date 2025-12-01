cedd6a6d7dfd6f079322286c9acebdfdfd4b1adf
Fazzi <faaris.ansari@proton.me>
2025-12-01 11:26:15 +0000

    pkgs/stremio-linux-shell: enable offline-build

--- parts/pkgs/packages/stremio-linux-shell.nix
+++ parts/pkgs/packages/stremio-linux-shell.nix
@@ -29,11 +29,13 @@
     pname = "cef-path";
     inherit (cef) version;
     dontUnpack = true;
+    # the exact files to copy are listed here:
+    # https://github.com/Stremio/stremio-linux-shell/blob/2eb0252fb568eaba829c9289e5ce49db6378f734/flatpak/com.stremio.Stremio.Devel.json#L140
     installPhase = ''
-      mkdir -p "$out"
-      cp -r ${cef}/Release/* $out
-      cp -r ${cef}/Resources/* $out
-      cp -r ${cef}/include $out
+      mkdir -p "$out/locales"
+      cp -r ${cef}/Release/{libcef.so,libEGL.so,libGLESv2.so,libvk_swiftshader.so,v8_context_snapshot.bin} $out
+      cp -r ${cef}/Resources/{*.pak,icudtl.dat} $out
+      cp -r ${cef}/Resources/locales/*.pak $out/locales
     '';
     postFixup = ''
       strip $out/*.so*
@@ -53,6 +55,10 @@ in
       "glutin-0.32.3" = "sha256-5IX+03mQmWxlCdVC0g1q2J+ulW+nPTAhYAd25wyaHx8=";
       "libmpv2-4.1.0" = "sha256-zXMFuajnkY8RnVGlvXlZfoMpfifzqzJnt28a+yPZmcQ=";
     };
+    # Don't fetch the CEF binaries at build time.
+    buildFeatures = [
+      "offline-build"
+    ];
 
     buildInputs = [
       openssl
@@ -74,6 +80,7 @@ in
       mv $out/bin/stremio-linux-shell $out/bin/stremio
       # copy vendored server.js, shell no longer fetches at runtime
       cp $src/data/server.js $out/bin/server.js
+
       cp $src/data/com.stremio.Stremio.desktop $out/share/applications/com.stremio.Stremio.desktop
       cp $src/data/icons/com.stremio.Stremio.svg $out/share/icons/hicolor/scalable/apps/com.stremio.Stremio.svg
 
