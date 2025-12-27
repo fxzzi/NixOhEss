f18ff4ae0964b1e4c8b209072aa685fe5091777d
Fazzi <faaris.ansari@proton.me>
2025-12-27 22:34:58 +0000

    fazziGO/kunzozPC: revert stremio shell

--- hosts/fazziGO/default.nix
+++ hosts/fazziGO/default.nix
@@ -14,7 +14,7 @@
       packages = with pkgs; [
         telegram-desktop
         deluge
-        self'.packages.stremio-linux-shell-rewrite
+        self'.packages.stremio-linux-shell
         # uni stuff
         onlyoffice-desktopeditors
         processing
