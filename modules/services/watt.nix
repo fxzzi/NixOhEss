9dad0780d24d244c629296fed23c1f422162078d
Fazzi <faaris.ansari@proton.me>
2026-02-26 18:52:13 +0000

    watt: use custom config

--- modules/services/watt.nix
+++ modules/services/watt.nix
@@ -13,6 +13,34 @@ in {
       power-profiles-daemon.enable = mkForce false;
       watt.enable = true;
     };
+    systemd.services.watt.serviceConfig.Environment = [
+      "WATT_CONFIG=/etc/watt.toml"
+    ];
+    environment.etc."watt.toml".text = mkForce ''
+      [[rule]]
+      name     = "discharging-power-saving"
+      if       = "?discharging"
+      priority = 99
+
+      cpu.energy-perf-bias              = { if.is-energy-perf-bias-available = "power", then = "power" }
+      cpu.energy-performance-preference = { if.is-energy-performance-preference-available = "power", then = "power" }
+      cpu.governor                      = { if.is-governor-available = "powersave", then = "powersave" }
+      cpu.turbo                         = { if = "?turbo-available", then = false }
+
+      power.platform-profile            = { if.is-platform-profile-available = "low-power", then = "low-power" }
+
+      [[rule]]
+      name     = "plugged-in-performance"
+      if.not   = "?discharging"
+      priority = 100
+
+      cpu.energy-perf-bias              = { if.is-energy-perf-bias-available = "performance", then = "performance" }
+      cpu.energy-performance-preference = { if.is-energy-performance-preference-available = "performance", then = "performance" }
+      cpu.governor                      = { if.is-governor-available = "performance", then = "performance" }
+      cpu.turbo                         = { if = "?turbo-available", then = true }
+
+      power.platform-profile            = { if.is-platform-profile-available = "performance", then = "performance" }
+    '';
   };
   imports = [inputs.watt.nixosModules.default];
 }
