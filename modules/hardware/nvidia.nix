ecb8c60a3cd04ab2b136fb755f9e7e462a79bb92
Fazzi <faaris.ansari@proton.me>
2026-03-07 00:10:35 +0000

    nvidia: amend module to support closed drivers

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -6,6 +6,7 @@
 }: let
   inherit (lib) mkEnableOption mkIf optionals getExe';
   cfg = config.cfg.hardware.nvidia;
+  isOpen = config.hardware.nvidia.open;
 in {
   options.cfg.hardware.nvidia = {
     exposeTemp = mkEnableOption "nvidia-temp";
@@ -18,20 +19,19 @@ in {
 
     hardware = {
       nvidia = {
-        # open drivers fail to wake from suspend for me :(
         open = true;
-        gsp.enable = config.hardware.nvidia.open; # if using closed drivers, lets assume you don't want gsp
-        powerManagement.enable = !config.hardware.nvidia.open;
+        gsp.enable = isOpen; # if using closed drivers, lets assume you don't want gsp
+        powerManagement.enable = !isOpen;
         nvidiaSettings = false; # useless on wayland still
-        # package = config.boot.kernelPackages.nvidiaPackages.beta;
+        package = config.boot.kernelPackages.nvidiaPackages.beta;
         # NOTE: if a new nvidia driver isn't in nixpkgs yet, use below
-        package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-          version = "595.45.04";
-          sha256_64bit = "sha256-zUllSSRsuio7dSkcbBTuxF+dN12d6jEPE0WgGvVOj14=";
-          openSha256 = "sha256-uqNfImwTKhK8gncUdP1TPp0D6Gog4MSeIJMZQiJWDoE=";
-          usePersistenced = false;
-          useSettings = false;
-        };
+        # package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        #   version = "595.45.04";
+        #   sha256_64bit = "sha256-zUllSSRsuio7dSkcbBTuxF+dN12d6jEPE0WgGvVOj14=";
+        #   openSha256 = "sha256-uqNfImwTKhK8gncUdP1TPp0D6Gog4MSeIJMZQiJWDoE=";
+        #   usePersistenced = false;
+        #   useSettings = false;
+        # };
       };
       graphics = {
         enable = true;
@@ -50,31 +50,28 @@ in {
         __EGL_VENDOR_LIBRARY_CONFIG_DIRS = "/run/opengl-driver/share/glvnd/egl_vendor.d/";
         # fix hw acceleration in bwrap (osu!lazer, wrapped appimages)
         __EGL_EXTERNAL_PLATFORM_CONFIG_DIRS = "/run/opengl-driver/share/egl/egl_external_platform.d/";
+        # avoid creation $HOME/.nv dir
         CUDA_CACHE_PATH = "$XDG_CACHE_HOME/nv";
         # stop forcing high GPU clocks when CUDA is in use
         CUDA_DISABLE_PERF_BOOST = 1;
       };
       etc = {
-        # fix high vram usage on some apps. nvidia tries to do this automatically but only for select programs
-        "nvidia/nvidia-application-profiles-rc.d/50-limit-free-buffer-pool.json".text = builtins.toJSON {
+        "nvidia/nvidia-application-profiles-rc.d/50-vram-and-cuda-fixes.json".text = builtins.toJSON {
           rules = [
             {
               pattern = {
                 feature = "true";
                 matches = "";
               };
+              # fix high vram usage on some apps. nvidia tries to do this automatically but only for select programs
               profile = "No VidMem Reuse";
             }
-          ];
-        };
-        # don't lock to a lower (p2) perf state when cuda is in use.
-        "nvidia/nvidia-application-profiles-rc.d/51-cuda-dont-lock-to-p2.json".text = builtins.toJSON {
-          rules = [
             {
               pattern = {
                 feature = "true";
                 matches = "";
               };
+              # don't lock to a lower (p2) perf state when cuda is in use.
               profile = "CudaNoStablePerfLimit";
             }
           ];
@@ -97,11 +94,12 @@ in {
           "nvidia.NVreg_EnableResizableBar=1" # enable reBAR
           "nvidia.NVreg_RegistryDwords=RmEnableAggressiveVblank=1" # low-latency stuff
           "nvidia-modeset.disable_vrr_memclk_switch=1"
-          "nvidia.NVreg_TemporaryFilePath=/var/tmp"
         ]
-        ++ optionals config.hardware.nvidia.open [
-          "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
+        ++ optionals isOpen [
           "nvidia.NVreg_UseKernelSuspendNotifiers=1"
+        ]
+        ++ optionals (!isOpen) [
+          "nvidia.NVreg_TemporaryFilePath=/var/tmp" # store on disk, not /tmp which is on RAM
         ];
     };
     systemd.services.nvidia-temp = let
