373c2efbd1bbe75832e798b7165f32f7acc6f249
Kunooz <syedkunooz@gmail.com>
2026-03-08 19:00:56 +0000

    kunzozPC: disable suspend

--- hosts/kunzozPC/options.nix
+++ hosts/kunzozPC/options.nix
@@ -45,7 +45,7 @@
         enable = true;
         dpmsTimeout = 600;
         lockTimeout = 620;
-        suspendTimeout = 1200;
+        # suspendTimeout = 1200;
       };
       hyprpaper.enable = true;
       xdph.enable = true;
