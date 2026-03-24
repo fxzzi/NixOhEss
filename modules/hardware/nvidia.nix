df042eb64caaa82079334b07a84b19b8d6d47972
Fazzi <faaris.ansari@proton.me>
2026-03-24 17:46:58 +0000

    nvidia: update to 595.45.04

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -24,15 +24,16 @@ in {
         gsp.enable = isOpen; # if using closed drivers, lets assume you don't want gsp
         powerManagement.enable = !isOpen;
         nvidiaSettings = false; # useless on wayland still
-        package = config.boot.kernelPackages.nvidiaPackages.beta;
+        # package = config.boot.kernelPackages.nvidiaPackages.beta;
         # NOTE: if a new nvidia driver isn't in nixpkgs yet, use below
-        # package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-        #   version = "595.45.04";
-        #   sha256_64bit = "sha256-zUllSSRsuio7dSkcbBTuxF+dN12d6jEPE0WgGvVOj14=";
-        #   openSha256 = "sha256-uqNfImwTKhK8gncUdP1TPp0D6Gog4MSeIJMZQiJWDoE=";
-        #   usePersistenced = false;
-        #   useSettings = false;
-        # };
+        package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+          version = "595.58.03";
+          sha256_64bit = "sha256-jA1Plnt5MsSrVxQnKu6BAzkrCnAskq+lVRdtNiBYKfk=";
+          sha256_aarch64 = "";
+          openSha256 = "sha256-6LvJyT0cMXGS290Dh8hd9rc+nYZqBzDIlItOFk8S4n8=";
+          settingsSha256 = "";
+          persistencedSha256 = "";
+        };
       };
       graphics = {
         enable = true;
