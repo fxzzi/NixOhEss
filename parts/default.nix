000e936e6c2abebc549a2040259ca2c886f73075
Fazzi <faaris.ansari@proton.me>
2026-04-16 04:50:16 +0100

    parts: cleanup all systems abstraction

--- parts/default.nix
+++ parts/default.nix
@@ -7,9 +7,11 @@
   pins = import ./npins;
 
   # we only use x86_64-linux for now but this is good practice
-  systems = import inputs.systems;
-  genSystems = genAttrs systems;
-  pkgsForEach = inputs.nixpkgs.legacyPackages;
+  supportedSystems = import inputs.systems;
+  forAllSystems = apply:
+    genAttrs
+    supportedSystems
+    (system: apply inputs.nixpkgs.legacyPackages.${system});
 in {
   # our private lib which has some generators and useful funcs
   lib = import ./lib lib;
@@ -18,17 +20,15 @@ in {
     inherit self inputs pins lib;
   };
 
-  packages = genSystems (system: let
-    pkgs = pkgsForEach.${system};
-  in
+  packages = forAllSystems (pkgs:
     # some of our pkgs depend on each other, so use fix and pass self through
-    fix (selfPkgs:
-      # recursively callPackage every drv in ./pkgs
-        packagesFromDirectoryRecursive {
-          # pass through our npins sources as well
-          callPackage = pkgs.lib.callPackageWith (pkgs // selfPkgs // {inherit pins;});
-          directory = ./pkgs;
-        }));
+      fix (selfPkgs:
+        # recursively callPackage every drv in ./pkgs
+          packagesFromDirectoryRecursive {
+            # pass through our npins sources as well
+            callPackage = pkgs.lib.callPackageWith (pkgs // selfPkgs // {inherit pins;});
+            directory = ./pkgs;
+          }));
 
-  formatter = genSystems (system: import ./fmt.nix {pkgs = pkgsForEach.${system};});
+  formatter = forAllSystems (pkgs: import ./fmt.nix {inherit pkgs;});
 }
