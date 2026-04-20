e0e4f384560de63c8b65f0bfce51665217f81569
Fazzi <faaris.ansari@proton.me>
2026-04-20 14:07:59 +0100

    gnome-keyring: manually run in sysd user service

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -3,9 +3,6 @@
     services = {
       # disable speech-dispatcher
       speechd.enable = false;
-      # for passwords
-      # FIXME: doesn't fucken auto unlock half the time????
-      gnome.gnome-keyring.enable = true;
     };
   };
 }
