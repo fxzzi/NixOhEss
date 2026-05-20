f04d1fea9c424d09e959b4bcdfccc32e7618628c
Fazzi <faaris.ansari@proton.me>
2026-05-20 21:08:21 +0100

    amdgpu: add low_latency_layer

--- modules/hardware/amdgpu.nix
+++ modules/hardware/amdgpu.nix
@@ -1,6 +1,8 @@
 {
   lib,
   config,
+  pkgs,
+  pins,
   ...
 }: let
   inherit (lib) mkEnableOption mkIf;
@@ -28,9 +30,16 @@ in {
     # enable HIP for packages which use it
     # (blender for the most part lol)
     nixpkgs.config.hipSupport = true;
-    environment.sessionVariables = {
-      # Increase AMD's shader cache size to xGB
-      MESA_SHADER_CACHE_MAX_SIZE = "12G";
+    environment = {
+      sessionVariables = {
+        # Increase AMD's shader cache size to xGB
+        MESA_SHADER_CACHE_MAX_SIZE = "12G";
+      };
+      systemPackages = [
+        (pkgs.callPackage "${pins.nix-gaming}/pkgs/low-latency-layer" {
+          pins = import "${pins.nix-gaming}/npins";
+        })
+      ];
     };
   };
 }
