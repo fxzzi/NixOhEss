abc9db8d65cdf5f40fe1777e1b03157322026af4
Fazzi <faaris.ansari@proton.me>
2026-05-25 00:59:28 +0200

    ags: poll date every 60s

--- modules/services/ags/modules/Date.js
+++ modules/services/ags/modules/Date.js
@@ -1,6 +1,6 @@
 const date = Variable("", {
   poll: [
-    360000,
+    60000,
     () => {
       const now = new Date();
       return `${now.toLocaleDateString(undefined, { weekday: "short" })} ${String(now.getDate()).padStart(2, "0")}/${String(now.getMonth() + 1).padStart(2, "0")}`;
