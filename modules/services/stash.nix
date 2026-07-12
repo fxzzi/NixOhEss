c06d269868d5688d9e784569bf4f388d01e0358b
Fazzi <faaris.ansari@proton.me>
2026-07-12 16:08:42 +0100

    oo7: init to replace gnome-keyring

--- modules/services/stash.nix
+++ modules/services/stash.nix
@@ -5,14 +5,14 @@
   ...
 }: let
   inherit (lib) mkEnableOption mkIf getExe;
-  inherit (pkgs) writeText stash-clipboard;
+  inherit (pkgs) writeText runCommand stash-clipboard;
   cfg = config.cfg.services.stash;
   regex = "(password|secret|api[_-]?key|token)[=: ]+[^\s]+";
 in {
   options.cfg.services.stash.enable = mkEnableOption "stash";
   config = mkIf cfg.enable {
     environment.systemPackages = [
-      (pkgs.runCommand "stash-symlinks" {} ''
+      (runCommand "stash-symlinks" {} ''
         mkdir -p $out/bin
         for bin in stash-copy stash-paste wl-copy wl-paste; do
           ln -s ${getExe stash-clipboard} $out/bin/$bin
