c9d6b3422423e671009f6334b790c16c0a70ca43
Fazzi <faaris.ansari@proton.me>
2026-07-12 01:28:14 +0100

    stash: symlink for wl-clipboard utils

--- modules/services/stash.nix
+++ modules/services/stash.nix
@@ -4,13 +4,21 @@
   pkgs,
   ...
 }: let
-  inherit (lib) mkEnableOption mkIf;
-  inherit (pkgs) writeText;
+  inherit (lib) mkEnableOption mkIf getExe;
+  inherit (pkgs) writeText stash-clipboard;
   cfg = config.cfg.services.stash;
   regex = "(password|secret|api[_-]?key|token)[=: ]+[^\s]+";
 in {
   options.cfg.services.stash.enable = mkEnableOption "stash";
   config = mkIf cfg.enable {
+    environment.systemPackages = [
+      (pkgs.runCommand "stash-symlinks" {} ''
+        mkdir -p $out/bin
+        for bin in stash-copy stash-paste wl-copy wl-paste; do
+          ln -s ${getExe stash-clipboard} $out/bin/$bin
+        done
+      '')
+    ];
     services.stash-clipboard = {
       enable = true;
       arguments = ["--max-items 10"];
