475df3a7d7dea14e0ed367fff6c6a7681d393b03
Fazzi <faaris.ansari@proton.me>
2026-07-15 12:43:43 +0100

    qt: use kvantum and kvlibadwaita for theme

--- modules/core/qt.nix
+++ modules/core/qt.nix
@@ -1,37 +1,54 @@
 {
   pkgs,
+  inputs,
   lib,
   ...
 }: {
   config = {
     environment.sessionVariables.QT_QPA_PLATFORMTHEME = "qt6ct";
     hj = {
-      xdg.config.files."qt6ct/qt6ct.conf" = {
-        generator = lib.generators.toINI {};
-        value = {
-          Appearance = {
-            icon_theme = "Papirus-Dark";
-            standard_dialogs = "xdgdesktopportal";
-            style = "Adwaita-dark";
+      xdg.config.files = {
+        "qt6ct/qt6ct.conf" = {
+          generator = lib.generators.toINI {};
+          value = {
+            Appearance = {
+              icon_theme = "Papirus-Dark";
+              standard_dialogs = "xdgdesktopportal";
+              style = "kvantum-dark";
+              custom_palette = true;
+            };
+            Fonts = {
+              fixed = ''"monospace,10,-1,5,400,0,0,0,0,0,0,0,0,0,0,1,Regular"'';
+              general = ''"sans-serif,10,-1,5,400,0,0,0,0,0,0,0,0,0,0,1,Regular"'';
+            };
           };
-          Fonts = {
-            fixed = ''"monospace,10,-1,5,400,0,0,0,0,0,0,0,0,0,0,1,Regular"'';
-            general = ''"sans-serif,10,-1,5,400,0,0,0,0,0,0,0,0,0,0,1,Regular"'';
-          };
+        };
+        "Kvantum/Colors".source = "${inputs.KvLibadwaita}/src/Colors";
+        "Kvantum/KvLibadwaita".source = "${inputs.KvLibadwaita}/src/KvLibadwaita";
+        "Kvantum/kvantum.kvconfig" = {
+          generator = lib.generators.toINI {};
+          value.General.theme = "KvLibadwaitaDark";
         };
       };
       packages = with pkgs; [
         (symlinkJoin {
-          inherit (pkgs.qt6Packages.qt6ct) name pname version meta;
-          paths = [pkgs.qt6Packages.qt6ct];
-          # remove the qt6ct .desktop file. It's not like
-          # we can modify settings in there anyway.
+          inherit (qt6Packages.qtstyleplugin-kvantum) name pname version meta;
+          paths = [qt6Packages.qtstyleplugin-kvantum];
+          # remove the desktop file. It's not like
+          # we can modify settings in it anyway.
+          postBuild = ''
+            unlink $out/share/applications/kvantummanager.desktop
+          '';
+        })
+        (symlinkJoin {
+          inherit (qt6Packages.qt6ct) name pname version meta;
+          paths = [qt6Packages.qt6ct];
+          # remove the desktop file. It's not like
+          # we can modify settings in it anyway.
           postBuild = ''
             unlink $out/share/applications/qt6ct.desktop
           '';
         })
-        adwaita-qt
-        adwaita-qt6
       ];
     };
   };
