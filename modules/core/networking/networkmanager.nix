c5707a8f6e74960cbf48e5175cf2c3060f278372
Fazzi <faaris.ansari@proton.me>
2026-09-01 22:39:14 +0500

    networkmanager: remove connectionConfig

--- modules/core/networking/networkmanager.nix
+++ modules/core/networking/networkmanager.nix
@@ -22,11 +22,6 @@ in
           powersave = config.cfg.core.isLaptop;
         };
         dns = "systemd-resolved";
-        connectionConfig = {
-          # don't use router advertised dns
-          "ipv4.ignore-auto-dns" = true;
-          "ipv6.ignore-auto-dns" = true;
-        };
         dhcp = "dhcpcd";
         plugins = with pkgs; [
           networkmanager-openvpn
