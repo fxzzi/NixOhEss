973af6d8bd2950789dbbcc85fe44bc6d5228b456
Fazzi <faaris.ansari@proton.me>
2026-09-10 17:09:27 +0300

    zram: fix comment formatting

--- modules/core/zram.nix
+++ modules/core/zram.nix
@@ -11,12 +11,12 @@
   boot = {
     kernelParams = [ "zswap.enabled=0" ];
     # - `vm.swappiness = 150` increase swapping aka compression to be
-    #  able to cache more file page data
+    #   able to cache more file page data
     # - `vm.watermark_boost_factor = 0` watermark boosting can cause unpredictable stalls as seen here:
-    #  https://bugs.launchpad.net/ubuntu/+source/linux/+bug/1861359
+    #   https://bugs.launchpad.net/ubuntu/+source/linux/+bug/1861359
     # - `vm.watermark_scale_factor = 125` initiate kswapd much earlier
-    # as zram will also apply pressure when requesting memory for
-    # compressed swap
+    #   as zram will also apply pressure when requesting memory for
+    #   compressed swap
     # - `vm.page-cluster = 0` disables page prefetching
     kernel.sysctl = {
       "vm.swappiness" = 150;
