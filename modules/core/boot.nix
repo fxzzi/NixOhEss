5d4dbe14d20c5d7ec5218a1c2255d11d335d548e
Fazzi <faaris.ansari@proton.me>
2026-09-10 16:25:25 +0300

    boot: only enable amd dynamic_epp on laptop

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -10,6 +10,7 @@ let
     mkOption
     types
     mkDefault
+    mkIf
     ;
 in
 {
@@ -41,7 +42,7 @@ in
         "fbcon=font:TER16x32" # make font size bigger
         "nowatchdog" # unsafe!! but fine for personal computers
         "mitigations=off" # also unsafe!!
-        "amd_pstate.dynamic_epp=1" # auto enable performance if plugged in
+        (mkIf config.cfg.core.isLaptop "amd_pstate.dynamic_epp=1") # auto enable performance if plugged in
       ];
       # disable some more watchdog
       extraModprobeConfig = ''
