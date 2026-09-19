9881a5d3cfb502502bb9079dd8a53775c153b0ae
Fazzi <faaris.ansari@proton.me>
2026-09-19 23:12:49 +0300

    tack: update

--- hosts/fazziGO/options.nix
+++ hosts/fazziGO/options.nix
@@ -48,6 +48,7 @@
     programs = {
       mangohud.enable = true;
       steam.enable = true;
+      proton-ge.enable = true;
       smoothScroll.enable = false;
       nvf.enable = true;
       adb.enable = true;
